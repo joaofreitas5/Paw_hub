28486218fe0ebdcc05c8bb5d98b65427afd119a8
joaofreitas5 <122179132+joaofreitas5@users.noreply.github.com>
2025-05-22 23:29:34 +0100

    Add files via upload

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router} from "@angular/router";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    private apiUrl = 'http://localhost:3000/api';
+
+    constructor(private http: HttpClient, private router: Router) {}
+
+    login(credentials: { username password: string }) {
