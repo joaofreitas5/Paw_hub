7b25abe70ddcb0eff31331794f31e05d3480f1e9
Mezikt97 <nunomesquita97@gmail.com>
2025-05-24 16:22:28 +0100

    Refactor menu management components and update routing
    - Moved menu-related components from 'menus' to 'menu-management' directory.
    - Updated routing paths to reflect new component locations.
    - Deleted old menu components (MenuCreate, MenuDetails, MenuEdit, MenuList) and their associated files.
    - Created new menu components (MenuCreateComponent, MenuDetailsComponent, MenuEditComponent, MenuListComponent) in the 'menu-management' directory with updated implementations.
    - Added basic HTML templates and CSS files for new components.
    - Updated unit tests for new components.
    - Removed unused components related to restaurant validation and user list.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-import { MenuListComponent } from './components/menus/menu-list/menu-list.component';
-import { MenuCreateComponent } from './components/menus/menu-create/menu-create.component';
-import { MenuDetailsComponent } from './components/menus/menu-details/menu-details.component';
-import { MenuEditComponent } from './components/menus/menu-edit/menu-edit.component';
+import { MenuListComponent } from './components/menu-management/menu-list/menu-list.component';
+import { MenuCreateComponent } from './components/menu-management/menu-create/menu-create.component';
+import { MenuDetailsComponent } from './components/menu-management/menu-details/menu-details.component';
+import { MenuEditComponent } from './components/menu-management/menu-edit/menu-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-import { MenuListComponent } from './components/menus/menu-list/menu-list.component';
-import { MenuCreateComponent } from './components/menus/menu-create/menu-create.component';
-import { MenuDetailsComponent } from './components/menus/menu-details/menu-details.component';
-import { MenuEditComponent } from './components/menus/menu-edit/menu-edit.component';
+import { MenuListComponent } from './components/menu-management/menu-list/menu-list.component';
+import { MenuCreateComponent } from './components/menu-management/menu-create/menu-create.component';
+import { MenuDetailsComponent } from './components/menu-management/menu-details/menu-details.component';
+import { MenuEditComponent } from './components/menu-management/menu-edit/menu-edit.component';
 import { authGuard } from './core/guard/auth.guard';
 
 
@@ -13,10 +13,10 @@ export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'menus', canActivate: [authGuard], loadComponent: () => import('./components/menus/menu-list/menu-list.component').then(m => m.MenuListComponent) },
-  { path: 'menus/new', canActivate: [authGuard], loadComponent: () => import('./components/menus/menu-create/menu-create.component').then(m => m.MenuCreateComponent) },
+  { path: 'menus', canActivate: [authGuard], loadComponent: () => import('./components/menu-management/menu-list/menu-list.component').then(m => m.MenuListComponent) },
+  { path: 'menus/new', canActivate: [authGuard], loadComponent: () => import('./components/menu-management/menu-create/menu-create.component').then(m => m.MenuCreateComponent) },
   { path: 'menus/:id', component: MenuDetailsComponent },
-  { path: 'menus/:id/edit', canActivate: [authGuard], loadComponent: () => import('./components/menus/menu-edit/menu-edit.component').then(m => m.MenuEditComponent) },
+  { path: 'menus/:id/edit', canActivate: [authGuard], loadComponent: () => import('./components/menu-management/menu-edit/menu-edit.component').then(m => m.MenuEditComponent) },
 ];
 
 
