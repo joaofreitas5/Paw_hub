cce3150b3309ac904224b1b6942ec1cefd66e741
Mezikt97 <nunomesquita97@gmail.com>
2025-05-28 16:27:50 +0100

    feat: update registration form with Material Design components; add role selection and improve styling

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { User } from '../../../models/user.model';
 
 @Component({
@@ -20,6 +21,7 @@ import { User } from '../../../models/user.model';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatSelectModule
   ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
