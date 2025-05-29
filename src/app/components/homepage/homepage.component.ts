68d82fb0c9b4559947d616f64a6d07c7dd9f7ea9
Mezikt97 <nunomesquita97@gmail.com>
2025-05-29 02:22:43 +0100

    feat: add dishes data to homepage component and update imports

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatCardModule],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
-  
+  pratos = [
+    {
+      nome: 'Frango Grelhado',
+      descricao: 'Suculento frango grelhado com ervas finas.',
+      imagem: 'https://source.unsplash.com/400x200/?grilled,chicken'
+    },
+    {
+      nome: 'Salmão ao Limão',
+      descricao: 'Salmão fresco com molho de limão e ervas.',
+      imagem: 'https://source.unsplash.com/400x200/?salmon,lemon'
+    },
+    {
+      nome: 'Massa Carbonara',
+      descricao: 'Massa italiana com molho cremoso e bacon.',
+      imagem: 'https://source.unsplash.com/400x200/?pasta,carbonara'
+    }
+  ];
 }
 
