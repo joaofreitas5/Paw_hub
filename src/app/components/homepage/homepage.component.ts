fc7f369591cb209fc1d5739ff3d2942aca5b2058
Mezikt97 <nunomesquita97@gmail.com>
2025-05-29 01:57:12 +0100

    feat: enhance homepage layout with toolbar and card components; update styles and content

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatToolbarModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
